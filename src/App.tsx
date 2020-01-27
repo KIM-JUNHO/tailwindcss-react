bc01bb49ee05675aca4c6cbf8f920f0fddb8cda3
Unknown <prpr48@naver.com>
2020-01-28 00:49:53 +0900

    btn-indigo, btn-gray

--- src/App.tsx
+++ src/App.tsx
@@ -25,8 +25,11 @@ const App: React.FC = () => {
               on vacation.
             </p>
             <div className="mt-4 sm:mt-6">
-              <a href="#" className="btn">
+              <a href="#" className="btn-indigo shadow-lg sm:text-base">
                 Book your escape
+              </a>
+              <a href="#" className="ml-2 btn-gray sm:text-base">
+                Learn more
               </a>
             </div>
           </div>
